c029b6742e79c7f0454431e89d69b4ae7d9381b0
clementlebas <38043935+clementlebas@users.noreply.github.com>
2022-04-30 15:32:17 +0200

    Delete globals.dart

--- lib/globals.dart
+++
@@ -1,7 +0,0 @@
-library globals;
-
-String codeAuth = '';
-String userId = '';
-String clientId = 'tob5oroyxr78eoi0fv7iqbltoe00ky';
-String clientSecret = 'g2zdlyxdgem1vni0acokbfnyu808hn';
-String redirectUri = 'https://clementlebas.fr/';
